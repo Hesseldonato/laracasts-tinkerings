b3082fd46b7a8ab3fda24cb9c46b1cb9f26b12c2
Bill Martin <bill@billmartin.io>
2016-10-30 20:28:57 -0400

    Convert cards controller to pull cards from a db model

--- first-project/app/Http/Controllers/CardsController.php
+++ first-project/app/Http/Controllers/CardsController.php
@@ -1,15 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Support\Facades\DB;
 use App\Http\Requests;
+use App\Card;
 
 class CardsController extends Controller
 {
     public function index()
     {
-        $cards = DB::table('cards')->get();
+        $cards = Card::all();
         return view('cards.index', compact('cards'));
     }
 }
