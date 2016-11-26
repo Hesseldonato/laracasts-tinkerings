cf6c6c71ee37799a71d3ea61a52c42fa3ae790cd
Bill Martin <bill@billmartin.io>
2016-11-25 22:37:35 -0500

    Add form for adding notes

--- first-project/resources/views/cards/show.blade.php
+++ first-project/resources/views/cards/show.blade.php
@@ -9,5 +9,16 @@
                 @endforeach
             </ul>
         </div>
+
+        <h3>Add a New Note</h3>
+
+        <form method="POST" action="/cards/{{ $card->id }}/notes">
+            <div class="form-group">
+              <textarea name="body" class="form-control"></textarea>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Add Note</button>
+            </div>
+        </form>
     </div>
 @stop
