fe631fbf1b84ee8263fd74843666e5577d980afe
Bill Martin <bill@billmartin.io>
2016-11-29 22:22:18 -0500

    Add code examples on using laravels service containers to implement singletons and factories

--- auth-project/routes/web.php
+++ auth-project/routes/web.php
@@ -17,6 +17,49 @@ Route::get('/begin', function () {
 });
 
 Route::get('/', function () {
+
+    /**
+     * Messy dependency injection example, of multiple dependencies having to be dealt with during instantiation
+     */
+    class UserRegistrar {
+        protected $mailer;
+
+        /**
+         * Example of constructor injection
+         * @param Mailer $mailer
+         */
+        public function __construct(Mailer $mailer)
+        {
+            $this->mailer = $mailer;
+        }
+    }
+    class Mailer {
+        protected $details = ['some', 'mailer', 'details'];
+    }
+
+    echo "<h3>See web.php routes source code for code examples for below var dumps:</h3>";
+
+    echo "Example of messy dependency injection being dealt with to use above classes: ";
+    $user_registrar = new UserRegistrar(new Mailer);
+    var_dump($user_registrar); echo "<br><br>";
+
+    echo "example of using a 'factory' instead, with Laravel's 'service container' to manage dependencies";
+    App::bind('user_registrar', function(){
+        return new UserRegistrar(new Mailer);
+    });
+
+    echo 'Create using above "factory"';
+    $user_registrar = App::make('user_registrar');
+    var_dump($user_registrar); echo "<br><br>";
+
+    echo 'Or create it using singletons, such as a DB connection';
+    App::bind('user_registrar_singleton', function(){
+        return new UserRegistrar(new Mailer);
+    });
+
+    $user_registrar = App::singleton('user_registrar_singleton');
+    var_dump($user_registrar); echo "<br><br>";
+
     return view('welcome');
 });
 
